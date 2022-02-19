9fe939d95881b48362b1f5ef586e9bf0dd1f07f2
Fang Ming <fangming1987200@gmail.com>
2022-02-19 14:07:54 +0800

    Add 1st case with 0km and 0min waiting time.

---
+++ app/src/main/kotlin/com/tdd/exercise/Problem10.kt
@@ -0,0 +1,7 @@
+package com.tdd.exercise
+
+object Problem10 {
+    fun taxiFare(mileage: Int, waitingTime: Int): Float {
+        return 0F
+    }
+}
--- app/src/test/kotlin/com/tdd/exercise/Problem10Spec.kt
+++ app/src/test/kotlin/com/tdd/exercise/Problem10Spec.kt
@@ -1,7 +1,11 @@
 package com.tdd.exercise
 
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
 
 class Problem10Spec : ShouldSpec({
 
+    should("return 0 when taxi runs 0 km and wait 0 mins") {
+        Problem10.taxiFare(0, 0) shouldBe 0
+    }
 })
