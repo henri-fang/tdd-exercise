b90a4b072bb4db644259e863bd7ef0be28aa2f46
Fang Ming <fangming1987200@gmail.com>
2022-02-19 13:33:28 +0800

    Improve test.

--- app/src/main/kotlin/com/tdd/exercise/Problem9.kt
+++ app/src/main/kotlin/com/tdd/exercise/Problem9.kt
@@ -2,6 +2,7 @@ package com.tdd.exercise
 
 class Problem9(private val userStore: List<Pair<String, String>>) {
     fun login(username: String, password: String): String {
-        return if (username != "test-username") "fail" else "success"
+        val user = userStore.find { username == it.first }
+        return if (user != null) "success" else "fail"
     }
 }
