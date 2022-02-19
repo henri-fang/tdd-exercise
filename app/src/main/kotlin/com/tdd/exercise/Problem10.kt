11bd09ed35200b7a6335255f436d98bdee58bfb6
Fang Ming <fangming1987200@gmail.com>
2022-02-19 14:30:44 +0800

    Refactor implementation codes.

--- app/src/main/kotlin/com/tdd/exercise/Problem10.kt
+++ app/src/main/kotlin/com/tdd/exercise/Problem10.kt
@@ -2,9 +2,11 @@ package com.tdd.exercise
 
 object Problem10 {
     fun taxiFare(mileage: Int, waitingTime: Int): Float {
-        if (mileage > 8) {
-            return mileage * 0.8f * (1f + 0.5f) + waitingTime * 0.25f
-        }
-        return mileage * 0.8f + waitingTime * 0.25f
+        val waitingPrice = 0.25f
+        val kilometerPrice = 0.8f
+        val kilometerPriceLongJourney = 1.2f
+        val waitingFee = waitingTime * waitingPrice
+        val mileageFee = if (mileage > 8) mileage * kilometerPriceLongJourney else mileage * kilometerPrice
+        return mileageFee + waitingFee
     }
 }
