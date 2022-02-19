c34dba6f2243c6450526e00f5ca2febeafa41b07
Fang Ming <fangming1987200@gmail.com>
2022-02-19 13:25:48 +0800

    Add test for login success.

--- app/src/main/kotlin/com/tdd/exercise/Problem9.kt
+++ app/src/main/kotlin/com/tdd/exercise/Problem9.kt
@@ -1,5 +1,7 @@
 package com.tdd.exercise
 
 object Problem9 {
-
+    fun login(username: String, password: String): String {
+        return "success"
+    }
 }
--- app/src/test/kotlin/com/tdd/exercise/Problem9Spec.kt
+++ app/src/test/kotlin/com/tdd/exercise/Problem9Spec.kt
@@ -1,7 +1,12 @@
 package com.tdd.exercise
 
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
 
 class Problem9Spec : ShouldSpec({
-
+    should("return success when username and password are matched") {
+        val username = "test-username"
+        val password = "test-password"
+        Problem9.login(username, password) shouldBe "success"
+    }
 })
