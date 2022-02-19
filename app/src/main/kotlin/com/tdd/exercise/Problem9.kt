23ae24dda704c5c0dac74d916b2219035f75891c
Fang Ming <fangming1987200@gmail.com>
2022-02-19 13:48:52 +0800

    Refactor with arrow.

--- app/src/main/kotlin/com/tdd/exercise/Problem9.kt
+++ app/src/main/kotlin/com/tdd/exercise/Problem9.kt
@@ -1,9 +1,11 @@
 package com.tdd.exercise
 
+import arrow.core.getOrElse
+import arrow.core.toOption
+
 class Problem9(private val userStore: List<Pair<String, String>>) {
     fun login(username: String, password: String): String =
-        userStore.find {
-            username == it.first && password == it.second
-        }?.let { "success" }
-            ?: let { "fail" }
+        userStore.find { username == it.first && password == it.second }.toOption()
+            .map { "success" }
+            .getOrElse { "fail" }
 }
